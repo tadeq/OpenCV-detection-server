f4b4ed3a08dd05ccf8ebbea6f3e1537245404203
Maciej Moskal <mmtm1097@gmail.com>
2019-01-06 22:56:55 +0100

    refactor, correct, add back button

--- motion_detection.py
+++ motion_detection.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, Response, request
 from imutils.video import VideoStream
 import imutils
-import numpy as np
 import cv2
 from multiprocessing import Process
 from multiprocessing import Queue
@@ -12,99 +11,103 @@ app = Flask(__name__)
 # run with raspberry pi usb camera or with built-in laptop webcam
 pi = False
 
-# detect with neural network or with haar cascades
-network = False
 
 @app.route('/')
 def index():
     return render_template('welcome_page.html')
 
+
 @app.route('/configure', methods=['GET', 'POST'])
 def configure():
     return render_template('configure.html')
 
+
 @app.route('/video_viewer_conf', methods=['GET', 'POST'])
-def video_viewer_conf():    
+def video_viewer_conf():
+    global rectangle
     if request.method == 'POST':
-        global rectangle
         json = request.get_json()
-        pixelFromX = int(json['pixelFromX'])
-        pixelFromY = int(json['pixelFromY'])
-        width = int(json['width'])
-        height = int(json['height']) 
+        pixel_from_x = json['pixelFromX']
+        pixel_from_y = json['pixelFromY']
+        width = json['width']
+        height = json['height']
+        [pixel_from_x, pixel_from_y] = [val if val != '' else '0' for val in [pixel_from_x, pixel_from_y]]
+        [width, height] = [val if val != '' else '5000' for val in [width, height]]
+        [pixel_from_x, pixel_from_y, width, height] = [int(val) for val in [pixel_from_x, pixel_from_y, width, height]]
+        [pixel_from_x, pixel_from_y] = [val if val < 5000 else 0 for val in [pixel_from_x, pixel_from_y]]
+        [width, height] = [val if val < 5000 else 5000 for val in [width, height]]
         if not rectangle.empty():
             rectangle.get()
-        rectangle.put([pixelFromX, pixelFromY, width, height])
+        rectangle.put([pixel_from_x, pixel_from_y, width, height])
         return Response()
     else:
-        return Response(draw_area(video_stream, rectangle),mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(draw_area(video_stream), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def draw_area(cam, rectangle):
-    while True:   
-        frame = None    
-        try:
-            if pi:
-                frame = cam.read()
-            else:
-                _, frame = cam.read()                
-        except:                
-            #One frame wasn't read properly            
-            pass
-        
-        if frame is not None:  
-            
-            if not rectangle.empty():                
-                rectV = rectangle.get() #rectV-current value of the rectangle
-                rectangle.put(rectV)   
-                #rectV[0]==pixelFromX; rectV[1]==pixelFromY; rectV[2]==width; rectV[3]==height            
-                cv2.rectangle(frame, (rectV[0], rectV[1]), (rectV[0] + rectV[2], rectV[1] + rectV[3]  ), (0, 255, 0), 2)  
-          
-            _, jpeg_frame = cv2.imencode('.jpg', frame)
-            bytes_frame = jpeg_frame.tobytes()
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + bytes_frame + b'\r\n\r\n')
 
 @app.route('/surveillance')
 def surveillance():
-    return render_template('surveillance.html')
+    return render_template('index.html')
+
 
 @app.route('/video_viewer', methods=['GET', 'POST'])
 def video_viewer():
     global p
-    global rectangle
     if request.method == 'POST':
         json = request.get_json()
         mode = json['mode']
         if mode == "on":
             if p is None or not p.is_alive():
                 print("[INFO] starting process...")
-                time.sleep(0.5)
-                p = Process(target=classify_frame, args=(input_queue, output_queue,rectangle,))
+                p = Process(target=classify_frame, args=())
                 p.daemon = True
                 p.start()
-            return Response(process_frame(video_stream, input_queue, output_queue, detections, rectangle),
+            return Response(process_frame(video_stream),
                             mimetype='multipart/x-mixed-replace; boundary=frame')
         else:
             p.terminate()
             return '', 204
-    else:        
-        return Response(process_frame(video_stream, input_queue, output_queue, detections, rectangle),
+    else:
+        return Response(process_frame(video_stream),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def classify_frame(input_queue, output_queue, rectangle):
-    first_frame = None
+
+def draw_area(cam):
+    while True:
+        frame = None
+        if pi:
+            frame = cam.read()
+        else:
+            _, frame = cam.read()
+
+        if frame is not None:
+            if not rectangle.empty():
+                det_area = rectangle.get()  # det_area-current value of the rectangle
+                rectangle.put(det_area)
+                # det_area[0]==pixel_from_x; det_area[1]==pixel_from_y; det_area[2]==width; det_area[3]==height
+                cv2.rectangle(frame, (det_area[0], det_area[1]), (det_area[0] + det_area[2], det_area[1] + det_area[3]),
+                              (0, 255, 0), 2)
+
+            _, jpeg_frame = cv2.imencode('.jpg', frame)
+            bytes_frame = jpeg_frame.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + bytes_frame + b'\r\n\r\n')
+
+
+def classify_frame():
+    global detections, first_frame
     while True:
         # check to see if there is a frame in our input queue
         if not input_queue.empty():
             frame = input_queue.get()
+
             if frame is not None:
                 if not rectangle.empty():
-                    rectV = rectangle.get() #rectV-current value of the rectangle
-                    rectangle.put(rectV) 
-                    #rectV[0]==pixelFromX; rectV[1]==pixelFromY; rectV[2]==width; rectV[3]==height  
-                    frame = frame[rectV[0]:rectV[0]+rectV[2],rectV[1]:rectV[1]+rectV[3] ]
+                    det_area = rectangle.get()  # det_area-current value of the rectangle
+                    rectangle.put(det_area)
+                    # det_area[0]==pixel_from_x; det_area[1]==pixel_from_y; det_area[2]==width; det_area[3]==height
+                    frame = frame[det_area[0]:det_area[0] + det_area[2], det_area[1]:det_area[1] + det_area[3]]
                 # resize the frame, convert it to grayscale, and blur it
-                #frame = imutils.resize(frame, width=500)
+                # frame = imutils.resize(frame, width=500)
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -112,11 +115,11 @@ def classify_frame(input_queue, output_queue, rectangle):
                 if first_frame is None:
                     first_frame = gray
                     continue
-                if len(frame)!=len(first_frame):
+                if len(frame) != len(first_frame):
                     continue
                 # compute the absolute difference between the current frame and first frame
                 frame_delta = cv2.absdiff(first_frame, gray)
-                thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+                thresh = cv2.threshold(frame_delta, 50, 255, cv2.THRESH_BINARY)[1]
 
                 # dilate the thresholded image to fill in holes, then find contours on thresholded image
                 thresh = cv2.dilate(thresh, None, iterations=2)
@@ -124,51 +127,51 @@ def classify_frame(input_queue, output_queue, rectangle):
                 detections = imutils.grab_contours(detections)
                 output_queue.put(detections)
 
-def process_frame(cam, input_queue, output_queue, detections, rectangle):
-    while True:        
-        try:
-            if pi:
-                frame = cam.read()
-            else:
-                _, frame = cam.read()                
-        except:                
-            #One frame wasn't read properly
-            pass
 
-        if frame is not None:     
+def process_frame(cam):
+    global detections
+    while True:
+        if pi:
+            frame = cam.read()
+        else:
+            _, frame = cam.read()
+
+        if frame is not None:
 
             if input_queue.empty():
                 input_queue.put(frame)
 
             if not output_queue.empty():
                 detections = output_queue.get()
-            
-            if not rectangle.empty():                
-                rectV = rectangle.get() #rectV-current value of the rectangle
-                rectangle.put(rectV)
-                #rectV[0]==pixelFromX; rectV[1]==pixelFromY; rectV[2]==width; rectV[3]==height
-                cv2.rectangle(frame, (rectV[0], rectV[1]), (rectV[0] + rectV[2], rectV[1] + rectV[3] ), (0, 0, 255), 2) 
-            
+
+            if not rectangle.empty():
+                det_area = rectangle.get()  # det_area-current value of the rectangle
+                rectangle.put(det_area)
+                # det_area[0]==pixel_from_x; det_area[1]==pixel_from_y; det_area[2]==width; det_area[3]==height
+                cv2.rectangle(frame, (det_area[0], det_area[1]), (det_area[0] + det_area[2], det_area[1] + det_area[3]),
+                              (0, 0, 255), 2)
+
             if detections is not None:
                 for d in detections:
-                    #if cv2.contourArea(d) > 500:
-                        # compute the bounding box for the contour, draw it on the frame
-                        (x, y, w, h) = cv2.boundingRect(d)
-                        if not rectangle.empty():
-                            rectVal = rectangle.get()
-                            rectangle.put(rectVal)
-                            cv2.rectangle(frame, (x+rectVal[0], y+rectVal[1]), (x+rectVal[0] + w, y+rectVal[0] + h), (0, 255, 0), 2)
-                        #else:    
-                            #cv2.rectangle(frame, (x, y), (x - int(w), y - int(h)), (0, 255, 0), 2)
-            
+                    # if cv2.contourArea(d) > 360:
+                    # compute the bounding box for the contour, draw it on the frame
+                    (x, y, w, h) = cv2.boundingRect(d)
+                    if not rectangle.empty():
+                        rect = rectangle.get()
+                        rectangle.put(rect)
+                        cv2.rectangle(frame, (x + rect[0], y + rect[1]), (x + rect[0] + w, y + rect[1] + h),
+                                      (0, 255, 0), 2)
+
             _, jpeg_frame = cv2.imencode('.jpg', frame)
             bytes_frame = jpeg_frame.tobytes()
             yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + bytes_frame + b'\r\n\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + bytes_frame + b'\r\n\r\n')
+
 
 if __name__ == '__main__':
     input_queue = Queue(maxsize=1)
-    output_queue = Queue(maxsize=1)    
+    output_queue = Queue(maxsize=1)
+    first_frame = None
     detections = None
     p = None
     rectangle = Queue(maxsize=1)
